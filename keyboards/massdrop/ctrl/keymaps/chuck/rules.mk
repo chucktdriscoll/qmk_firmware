fa18b6cb178131f6649365e0692362c4b2df9b05
Charles Driscoll <charles.driscoll@savant.com>
2020-11-09 08:59:39 -0500

    1Add rules.mk for chuck keymap

--- keyboards/massdrop/ctrl/keymaps/chuck/rules.mk
+++ keyboards/massdrop/ctrl/keymaps/chuck/rules.mk
@@ -3,7 +3,7 @@
 # BOOTMAGIC_ENABLE = full		# Virtual DIP switch configuration
 # BOOTMAGIC_ENABLE = lite
 # AUTO_SHIFT_ENABLE = yes      	# Auto Shift
-NKRO_ENABLE = no  	         	# USB Nkey Rollover
+NKRO_ENABLE = yes  	         	# USB Nkey Rollover
 DYNAMIC_MACRO_ENABLE = yes		# Dynamic macro recording and play
 # MOUSEKEY_ENABLE = yes			# Enable mouse control keycodes. Increases firmware size.
 # TAP_DANCE_ENABLE = yes		# Enable tap dance keys
